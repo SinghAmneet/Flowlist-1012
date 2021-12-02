14457028763a04a95f5572afd8639667fe7239db
Susan-Ngoc Nguyen <91854784+snguuyen@users.noreply.github.com>
2021-12-02 01:32:28 -0500

    Update list.js

--- Inbox/List/list.js
+++ Inbox/List/list.js
@@ -1,7 +1,79 @@
-/*
-var inputBox = document.getElementById(".inputList input");
-var addButton = document.getElementById(".inputList button");
-var toDoList = document.getElementById(".todoList");
-var deleteBtn = document.getElementById(".footer button");
 
- */
+var inputBox = document.querySelector(".inputList input");//variable for the input box with "Create a list"
+var addButton = document.querySelector(".inputList button"); // variable for the "+" button
+var toDoList = document.querySelector(".toDoList"); //variable for class "toDoList" that contains empty lists (and the trash icons on the right)
+var deleteBtn = document.querySelector(".footer button"); // variable for the "Clear All button"
+
+// making input box editable by user to enter their tasks
+//when user enters characters in the input box, the onkeyup event executes
+inputBox.onkeyup = function(){
+    // .value returns what the user enters
+    inputBox.value;
+    
+  }
+
+showLists(); //show lists
+
+//when user clicks on the "+" button
+addButton.onclick = function() { 
+    //returns the value of the user's
+  var inputValue = inputBox.value; 
+  var FlowlistStorage = localStorage.getItem("New List"); //use localStorage object to store data with the id "New List" so that when the browser closes, the data doesn't get lost
+  if(FlowlistStorage == 0){ //if localStorage has no data, then we create an empty array so users' lists can show in vertical format
+    var arrayList = []; 
+  }else{
+    arrayList = JSON.parse(FlowlistStorage);  //convert JSON string into JS object
+  }
+ //push the users' entered list into the empty array
+    arrayList.push(inputValue); 
+  localStorage.setItem("New List", JSON.stringify(arrayList)); //convert back to JSON string
+
+  showLists(); // show lists 
+ 
+}
+
+function showLists(){
+  var FlowlistStorage = localStorage.getItem("New List");
+  if(FlowlistStorage == 0){
+    arrayList = [];
+  }else{
+   arrayList = JSON.parse(FlowlistStorage); 
+  }
+  var pendingLists = document.querySelector(".pendingLists");
+  pendingLists.textContent = arrayList.length; 
+  if(arrayList.length > 0){ //if array length is greater than 0
+    deleteBtn.classList.add();
+  }else{
+    deleteBtn.classList.remove();
+  }
+  //to create a list with the trash icon after the user inputs 
+  var newLists = "";
+  //this subsitutes the HTML empty li content with the user's lists, with the trash icons on the right side 
+arrayList.forEach(function(element, index)
+  {
+    newLists = newLists + `<li>${element}<span class="icon" onclick="deleteList(${index})"><i class='bx bxs-trash-alt'></i></span></li>`;
+  });
+
+  toDoList.innerHTML = newLists; 
+  inputBox.value = ""; //this clears the input field after user adds a list
+}
+
+
+function deleteList(index){
+  var FlowlistStorage = localStorage.getItem("New List");
+    arrayList = JSON.parse(FlowlistStorage);
+  arrayList.splice(index, 1); 
+  localStorage.setItem("New List", JSON.stringify(arrayList)); //this allows user to delete lists using the pink trash icon on the right
+  showLists(); //show lists
+}
+
+deleteBtn.onclick = function(){ //when "Clear All" button is clicked
+ arrayList = []; //empty out the array so it can restart
+  localStorage.setItem("New List", JSON.stringify(arrayList)); //set the item in localstorage
+  showLists(); // show lists
+}
+//add last modified date next to "Date created"
+ d =document.lastModified;
+   document.getElementById("listDate").innerHTML=d;
+
+  
