26f706d47f5f96d79f41c11b1a5866adfaed4927
ASofficial06 <87550709+ASofficial06@users.noreply.github.com>
2021-12-04 22:35:23 -0500

    Finalized and connected each file

---
+++ Sidebar/sidebar.js
@@ -0,0 +1,24 @@
+// Flowlist JS Sidebar File
+
+// querySelector() is a method of the element interface. It allows you yo find the first element that matches one or more CSS selectors
+window.onload = function(){
+    var btn = document.querySelector("#button_sidebar");
+    var sidebar = document.querySelector(".sidebar");
+
+    btn.onclick = function(){
+    // classList is a DOM property that returns class name of an element as a DOMTokenList object. This property is using the toggle method. More info here: https://www.w3schools.com/jsref/prop_element_classlist.asp
+    // When you click on sidebar menu icon on the top, it will remove (not really, returns a false value) sidebar class elements, but will add returns true)(closed) sidebar class elements if you click the icon again (Expanded)
+    sidebar.classList.toggle("active_sidebar");
+    }
+}
+
+function inbox_button(){
+    $(function(){
+        $("#component_content").load("/Inbox/inbox.html"); 
+    });
+
+}
+
+function later_development(){
+    alert("Error:\nSorry, we're still developing this section of the page :(\nPage will be implemented in the future.")
+}
--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 // Flowlist Server
 const url = require("url");
 const fs = require("fs");
+const port = 3000;
 const http = require("http");
 const lookup = require("mime-types").lookup;
 
 const server = http.createServer((req, res) => {
   let parsedURL = url.parse(req.url, true);
-  let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
-  if (path == "") {
-    path = "main_index.html";
+  let pathway = parsedURL.pathway.replace(/^\/+|\/+$/g, "");
+  if (pathway == "") {
+    pathway = "main_index.html";
   }
-  console.log(`Requested path ${path} `);
+  console.log(`Requested pathway ${pathway} `);
 
-  let file = __dirname + path;
+  let file = __dirname + pathway;
   fs.readFile(file, function(err, content) {
     if (err) {
       console.log(`File Not Found ${file}`);
       res.writeHead(404);
       res.end();
     } else {
-      console.log(`Returning ${path}`);
+      console.log(`Returning ${pathway}`);
       res.setHeader("X-Content-Type-Options", "nosniff");
-      let mime = lookup(path);
-      res.writeHead(200, { "Content-type": mime });
+      let mime = lookup(pathway);
+      res.writeHead(200, {"Content-type": mime});
       res.end(content);
     }
   });
 });
 
-server.listen(1234, "localhost", () => {
-  console.log("Listening on port 1234");
+server.listen(port, "localhost", () => {
+  console.log("Listening on port "+ port);
 });
